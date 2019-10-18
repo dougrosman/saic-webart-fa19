a145de013db0dafed72c085b78cf168771fed7b5
Doug Rosman <drosman@saic.edu>
2019-10-18 00:51:18 -0500

    update

--- docs/playgrounds/LeftScroll/left-scroll.js
+++ docs/playgrounds/LeftScroll/left-scroll.js
@@ -2,7 +2,9 @@
 let pages = document.getElementsByClassName("page");
 
 let navLinks = document.getElementsByClassName("nav-a");
+let closers = document.getElementsByClassName("close");
 
+let order = 0;
 
 for(let i = 0; i < navLinks.length; i++) {
     navLinks[i].addEventListener("click", function(e){
@@ -10,20 +12,25 @@ for(let i = 0; i < navLinks.length; i++) {
         console.log(pages[i]);
 
         if(pages[i].style.display == "block") {
-            // pages[i].style.animationName = "fade-out";
-            // setTimeout(function() {
-            //     pages[i].style.display = "none";
-            // }, 1000);
             return;
-            
-            
         } else {
-            
+            order++;
             pages[i].style.animationName = "fade-in";
+            pages[i].style.order = `${order}`;
             pages[i].style.display = "block";
             pages[i].scrollIntoView();
             
         }
+    });
+    
+    closers[i].addEventListener("click", function(e) {
+
+        pages[i].style.animationName = "fade-out";
+            setTimeout(function() {
+                pages[i].style.display = "none";
+            }, 1000);
+        order--;
+        console.log(order);
     });
 }
 
