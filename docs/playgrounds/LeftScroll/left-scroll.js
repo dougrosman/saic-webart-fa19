11fe7ac750f80b481953b237b6c68e7f79cf706d
Doug Rosman <drosman@saic.edu>
2019-10-18 03:03:45 -0500

    update

--- docs/playgrounds/LeftScroll/left-scroll.js
+++ docs/playgrounds/LeftScroll/left-scroll.js
@@ -3,34 +3,47 @@ let pages = document.getElementsByClassName("page");
 
 let navLinks = document.getElementsByClassName("nav-a");
 let closers = document.getElementsByClassName("close");
-
 let order = 0;
 
 for(let i = 0; i < navLinks.length; i++) {
     navLinks[i].addEventListener("click", function(e){
         e.preventDefault();
+        console.log("added:");
         console.log(pages[i]);
 
         if(pages[i].style.display == "block") {
             return;
         } else {
-            order++;
+            
+            let orderDisplay = pages[i].querySelector('p');
             pages[i].style.animationName = "fade-in";
             pages[i].style.order = `${order}`;
+            order++;
             pages[i].style.display = "block";
+            orderDisplay.innerHTML = `<span style="font-size: 4em;">${pages[i].style.order}</span>`;
             pages[i].scrollIntoView();
+            
             
         }
     });
     
     closers[i].addEventListener("click", function(e) {
-
+        console.log("removed:");
         pages[i].style.animationName = "fade-out";
             setTimeout(function() {
                 pages[i].style.display = "none";
             }, 1000);
-        order--;
-        console.log(order);
+            order--;
+
+            for(let i = 0; i < navLinks.length; i++) {
+                console.log(pages[i]);
+                if(pages[i].style.display=='block' && pages[i].style.order >= order-1) {
+                    pages[i].style.order = `${pages[i].style.order-1}`;
+                    let orderDisplay = pages[i].querySelector('p');
+                    orderDisplay.innerHTML = `<span style="font-size: 4em;">${pages[i].style.order}</span>`;
+                    
+                }
+            }
     });
 }
 
